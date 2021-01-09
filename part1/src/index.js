c3bac8248f6d1b93fd87057f56957c08d0dac3d4
Paul K <azn.baoul@gmail.com>
2021-01-09 18:08:22 +0100

    init index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const Hello = (props) => {
-  const { name, age } = props;
-  return [
-    <p>
-      Hello {name}, you are {age} years old
-    </p>,
-    <p>Goodbye</p>,
-  ];
-};
+
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="George" age={age} />
-      <Hello name={name} age={25 + 10} />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
